fc7d05d9715b40acdea6ea25daa9e249f1f4f75c
Amanda Lowell <amandalowell2016@gmail.com>
2019-06-13 16:30:33 -0500

    firebase

---
+++ app.js
@@ -0,0 +1,13 @@
+ // Initialize Firebase
+  var firebaseConfig = {
+    apiKey: "AIzaSyB055Th0PsU5RJO5K9iGE5DNgxSz_lHMZo",
+    authDomain: "scheduler-271a0.firebaseapp.com",
+    databaseURL: "https://scheduler-271a0.firebaseio.com",
+    projectId: "scheduler-271a0",
+    storageBucket: "scheduler-271a0.appspot.com",
+    messagingSenderId: "187606175557",
+    appId: "1:187606175557:web:d02c5f818d9da80a"
+  };
+  // call the initiation
+  firebase.initializeApp(firebaseConfig);
+
