525f3ecf6a2c15863a6c11c882ee2ec2a24ff3fc
Amanda Lowell <amandalowell2016@gmail.com>
2019-06-13 22:03:19 -0500

    new

--- app.js
+++ app.js
@@ -62,13 +62,87 @@ var mockTrains = [
 // jumbotron bootstrap
 
 
-function countTrainTime(train){
-  var currentTime = new Date().getTime(); 
-  console.log(currentTime, train.firstTrainTime);
-}
+//function countTrainTime(train){
+ // var currentTime = new Date().getTime(); 
+//  console.log(currentTime, train.firstTrainTime);
+//}
 
-countTrainTime(mockTrains[0]);
+//countTrainTime(mockTrains[0]);
 
 
 
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = 0;
+
+
+// FUNCTIONS + EVENTS
+$("#addTrain").on("click", function() {
+
+  trainName = $('#nameInput').val().trim();
+  destination = $('#destinationInput').val().trim();
+  firstTrainTime = $('#firstTrainInput').val().trim();
+  frequency = $('#frequencyInput').val().trim();
+
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTrainTime);
+  console.log(frequency);
+
+  database.ref().push({
+    trainName: trainName,
+    destination: destination,
+    firstTrainTime: firstTrainTime,
+    frequency: frequency
+  });
+
+    return false;
+});
+
+
+// MAIN PROCESS + INITIAL CODE
+database.ref().on("child_added", function(snapshot) {
+  console.log(snapshot.val());
+
+  
+  trainName = snapshot.val().trainName;
+  destination = snapshot.val().destination;
+  firstTrainTime = snapshot.val().firstTrainTime;
+  frequency = snapshot.val().frequency;
+
+
+  
+  var firstTrainMoment = moment(firstTrainTime, 'HH:mm');
+  var nowMoment = moment(); 
+
+  var minutesSinceFirstArrival = nowMoment.diff(firstTrainMoment, 'minutes');
+  var minutesSinceLastArrival = minutesSinceFirstArrival % frequency;
+  var minutesAway = frequency - minutesSinceLastArrival;
+
+  var nextArrival = nowMoment.add(minutesAway, 'minutes');
+  var formatNextArrival = nextArrival.format("HH:mm");
+
+
+  
+  var tr = $('<tr>');
+  var a = $('<td>');
+  var b = $('<td>');
+  var c = $('<td>');
+  var d = $('<td>');
+  var e = $('<td>');
+  a.append(trainName);
+  b.append(destination);
+  c.append(frequency);
+  d.append(formatNextArrival);
+  e.append(minutesAway);
+  tr.append(a).append(b).append(c).append(d).append(e);
+  $('#newTrains').append(tr);
+
+
+  
+
+});
 
